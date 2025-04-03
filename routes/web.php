cabdf72ce1bd966a9f7522f5e0492a5d6d9fd65c
Natygabriela <nataliaggb007@gmail.com>
2025-04-03 13:57:59 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Livewire\Admin\Dashboard;
+use App\Livewire\Auth\Login;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', Login::class)->name('login');
+
+Route::get('/dashboard', Dashboard::class)->middleware(['auth', 'user_type:admin'])
+->name('admin.dashboard');
